35dfffa66120e6239da015966d4901147efde316
Fh Ndiritu <fhndiritu@gmail.com>
2023-10-13 22:37:54 +0300

    testing the posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
     validates :title, presence: true
-    validates :content, presence: true
+    validates :content, presence: true, length: {in: 3..10}
+
+
+    def summary
+        "#{content[0...20]}..."
+    end
 end
