6a38ab1c74b9f6c1d7b656ab595141b1dd247f78
Fh Ndiritu <fhndiritu@gmail.com>
2023-11-01 21:09:29 +0300

    seed dev db with test data

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :rooms
 
-  root to: "cars#index"
+  root to: "electronics#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
