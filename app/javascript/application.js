835a2278255877d07f764723ae75dba352550015
Fh Ndiritu <fhndiritu@gmail.com>
2023-11-01 21:46:26 +0300

    disabling turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import {turbo} from "@hotwired/turbo-rails"
+
+// turbo.session.drive = false
