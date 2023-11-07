3a4c7494fa4e18090bd284a2992069cbeb27a755
Fh Ndiritu <fhndiritu@gmail.com>
2023-11-07 14:52:52 +0300

    refactor, update and delete channels

--- app/models/electronic.rb
+++ app/models/electronic.rb
@@ -4,5 +4,9 @@ class Electronic < ApplicationRecord
     default_scope { order(id: :desc)}
 
 
-    after_create_commit ->{ broadcast_prepend_to "electronics", partial: "electronics/electronic", locals: { electronic: self}, target: "electronics" }
+    # after_create_commit ->{ broadcast_prepend_to "electronics", partial: "electronics/electronic", locals: { electronic: self}, target: "electronics" }
+    after_create_commit ->{ broadcast_prepend_to "electronics"}
+    after_update_commit ->{broadcast_update_to "electronics"}
+    after_destroy_commit ->{broadcast_remove_to "electronics"}
+
 end
