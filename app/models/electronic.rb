f665c15f1cacee3a433038ed2ae7d399c130796c
Fh Ndiritu <fhndiritu@gmail.com>
2023-11-07 14:45:25 +0300

    broadcast on actioncable channels

--- app/models/electronic.rb
+++ app/models/electronic.rb
@@ -1,3 +1,8 @@
 class Electronic < ApplicationRecord
     validates :name, presence: true, length: {in: 4...70}
+
+    default_scope { order(id: :desc)}
+
+
+    after_create_commit ->{ broadcast_prepend_to "electronics", partial: "electronics/electronic", locals: { electronic: self}, target: "electronics" }
 end
