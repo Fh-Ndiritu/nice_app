b6fd88b2f6de662a77ed784646685f6a5fa74ce3
Fh Ndiritu <fhndiritu@gmail.com>
2023-11-16 15:36:57 +0300

    Time for Stimulus :) :)

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  initialize(){
+    this.nameElement.value = this.name
+    console.log(this.targets.find("name").value)
+  }
+
+  log_me(){
+    var val = this.nameElement.value
+    console.log(val)
+  }
+
+  paste(event){
+    event.preventDefault()
+    alert("pasting not allowed")
+  }
+
+  get name(){
+    if(this.data.has("name")){
+      return this.data.get("name")
+    }else{
+      return "Just A user"
+    }
+  }
+
+  get nameElement(){
+    return this.targets.find("name")
   }
 }
