7e31fdbcf4fd40ac36ddc177f7330619f8263dc9
Fh Ndiritu <fhndiritu@gmail.com>
2023-11-19 10:53:04 +0300

    conditional rendering in css stimulus

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,10 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="copy-clipboard"
+// Connects to data-controller="clipboard"
 export default class extends Controller {
-
   static targets = ['source']
+  static classes = ['supported']
+
+  connect(){
+    if("clipboard" in navigator){
+      this.element.classList.add(this.supportedClass)
+    }
+  }
+
   copy(){
     navigator.clipboard.writeText(this.sourceTarget.value)
   }
+
+
 }
